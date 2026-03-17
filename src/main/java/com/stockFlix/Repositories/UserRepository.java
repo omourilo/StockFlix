9cb6f176d0d4336af63a56e9321c125d368d5503
Ramon Albini <166454486+Ramon-Albini@users.noreply.github.com>
2026-03-17 11:58:14 -0300

    Update: atualização dos comentários de UserRepository.java

--- src/main/java/com/stockFlix/Repositories/UserRepository.java
+++ src/main/java/com/stockFlix/Repositories/UserRepository.java
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import com.stockFlix.Models.Usuario;
 
+/**
+ * Repositório responsável pela interação com o banco de dados.
+ *
+ * Gerencia a persistencia de dados no banco da classe Usuario estendendo JpaRepository, 
+ * interface padrão SpringBoot com as operações CRUD já implementadas 
+ *
+ * @see JpaRepository
+ */
 @Repository
 public interface UserRepository extends JpaRepository<Usuario, Long>{
 
