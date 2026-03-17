090463a37979535bf91d3c230f4a57c378207fc5
'Ramon-Albini' <'ramonalbini13@gmail.com'>
2026-03-17 20:24:32 -0300

    feat: adicionando a conexão com o banco de dados

--- src/main/java/com/stockFlix/FlixApplication.java
+++ src/main/java/com/stockFlix/FlixApplication.java
@@ -3,10 +3,17 @@ package com.stockFlix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class FlixApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+	    dotenv.entries().forEach(e -> 
+	        System.setProperty(e.getKey(), e.getValue())
+	    );
 		SpringApplication.run(FlixApplication.class, args);
 	}
 
