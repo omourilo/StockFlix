04bf47d888e172686d97d242cafc1b5a64c809a1
Ramon-Albini <ramonalbini13@gmail.com>
2026-03-17 21:32:41 -0300

    feat: criação da exceção NotFoundException

---
+++ src/main/java/com/stockFlix/Excecoes/NotFoundException.java
@@ -0,0 +1,19 @@
+package com.stockFlix.Excecoes;
+
+
+/**
+ * Classe para lançamento de exceção para busca sem resultado.
+ * 
+ * Caso alguma busca no banco não encontre resultado, 
+ * temos essa exceção específica. 
+ * Facilitando o tratamento de erros e o teste unitario.
+ */
+public class NotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public NotFoundException(String warning) {
+		super(warning);
+	}
+
+}
