5ce4536e9f680b09e6b3b5ca2f91ba9e1325674b
Ramon Albini <166454486+Ramon-Albini@users.noreply.github.com>
2026-04-10 18:10:13 +0000

    feat: teste para criação de usuario

--- src/test/java/com/stockFlix/serviceTests/UsuarioServiceTest.java
+++ src/test/java/com/stockFlix/serviceTests/UsuarioServiceTest.java
@@ -2,15 +2,20 @@ package com.stockFlix.serviceTests;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import com.stockFlix.usuario.UsuarioService;
-import com.stockFlix.usuario.UsuarioRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.stockFlix.excecoes.LoginAlreadyExistsException;
 import com.stockFlix.usuario.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,7 +32,7 @@ class UsuarioServiceTest {
     @Test
     void testCreateUsuario() {
         // Arrange
-        UsuarioDTO usuarioDTO = new UsuarioDTO("Ramon@email", "1234", true);
+        UsuarioDTO usuarioDTO = new UsuarioDTO("Ramon@email", "1234", true); 
 
         Usuario usuarioEntity = new Usuario(1L, "Ramon@email", "1234", true);
 
@@ -42,5 +47,20 @@ class UsuarioServiceTest {
         assertEquals(true, resultadoDTO.acessoADM());
 
     }
+
+    @Test
+    void testCreateUsuarioLoginJaExiste() {
+        UsuarioDTO usuarioDTO = new UsuarioDTO("Ramon@email", "1234", true);
+
+        Usuario usuarioEntity = new Usuario();
+
+        when(usuarioRepo.findByLogin(anyString())).thenReturn(Optional.of(usuarioEntity));
+        
+
+        LoginAlreadyExistsException ex = assertThrows(LoginAlreadyExistsException.class, () -> usuarioService.createUsuario(usuarioDTO));
+        System.err.println(ex.getMessage());
+    }
+
+    
 
 }
