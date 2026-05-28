e11ac593a441584d82ae4c021293fc9c4f120228
Ramon Albini <166454486+Ramon-Albini@users.noreply.github.com>
2026-05-28 13:43:45 +0000

    feat: previsaoServicetest

--- src/test/java/com/stockFlix/serviceTests/PrevisaoServiceTest.java
+++ src/test/java/com/stockFlix/serviceTests/PrevisaoServiceTest.java
@@ -1,35 +1,31 @@
 package com.stockFlix.serviceTests;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.stockFlix.movimentacao.MovimentacaoRepository;
+import com.stockFlix.previsao.PrevisaoRepository;
+import com.stockFlix.previsao.PrevisaoService;
+import com.stockFlix.produto.ProdutoRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class PrevisaoServiceTest {
-    @Test
-    public void testCalculoPrevisao() {
+    
+    @Mock
+    private ProdutoRepository produtoRepo;
+    @Mock
+    private PrevisaoRepository previsaoRepo;
+    @Mock
+    private MovimentacaoRepository movimentacaoRepo;
 
-    }
+    @InjectMocks
+    private PrevisaoService previsaoService;
 
-    @Test
-    public void testCreatePrevisao() {
 
-    }
+    
 
-    @Test
-    public void testDeletePrevisao() {
-
-    }
-
-    @Test
-    public void testGetAllPrevisoes() {
-
-    }
-
-    @Test
-    public void testGetAllPrevisoesByProdutoId() {
-
-    }
-
-    @Test
-    public void testGetPrevisaoById() {
-
-    }
+    
 }
